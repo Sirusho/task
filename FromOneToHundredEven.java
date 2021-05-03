01cb422cd3f89c7f0a9f5da7528c09a52a4e73d8
Siranush Kamavosyan <sirushokamavosyan@gmail.com>
2021-05-03 12:26:06 +0400

    new tasks added

--- FromOneToHundredEven.java
+++ FromOneToHundredEven.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class FromOneToHundredEven {
+    public static void main(String[] args) {
+        int n=0;
+        while(n<=100){
+            System.out.print(n + " ");
+            n+=2;
+        }
+
+    }
 }
